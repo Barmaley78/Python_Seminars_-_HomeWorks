6d35205aee89db159efe4a6f400441d1d43ec1ed
Sergey <barmaley78@mail.ru>
2023-01-20 17:29:50 +0700

    исправлена задача с 6 семинара

--- seminar6/ex4/ex4.py
+++ seminar6/ex4/ex4.py
@@ -12,9 +12,7 @@
 # 0 0 0 1
 
 n = int(input('введите размерность '))
-# list = [[int(i==j) for j in range(n)] for i in range(n)]
-k = 0
-list = [[(int(i)+1)*(int(j)+1) for j in range(n)] for i in range(n)]
+list = [[int(i==j) for j in range(n)] for i in range(n)]
 for i in range(n):
     for j in range(n):
         print((list[i][j]), end=' ')
