246bca3d84e04a973f25c744890e5b5f8f5c8106
Sergey <barmaley78@mail.ru>
2023-07-14 08:11:11 +0700

    toket deleted

--- hw9/bot.py
+++ hw9/bot.py
@@ -2,7 +2,7 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 from bot_commands import *
 
-app = ApplicationBuilder().token("6036630738:AAF5N2qqGV-NRRny9uSt2KFqQSo7QzAGQhY").build()
+app = ApplicationBuilder().token("").build()
 
 app.add_handler(CommandHandler("hello", hello_com))
 app.add_handler(CommandHandler("date", date_com))
